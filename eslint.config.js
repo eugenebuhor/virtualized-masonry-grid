429855b80361009b60e3fd75d50aea09ebd4869f
beugene <eugene.bugor@gmail.com>
2024-11-16 18:30:24 +0100

    Chore: lint-staged setup (#1)
    * chore: add lint-staged and prettier setup
    
    * chore: add husky pre-commit hook
    
    * chore: code clean up with eslint --fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,13 +17,23 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
+        'error',
+        {
+          tabWidth: 2,
+          printWidth: 100,
+          singleQuote: true,
+          jsxSingleQuote: false,
+          trailingComma: 'all',
+          bracketSpacing: true,
+          semi: true,
+        },
       ],
     },
   },
-)
+);
