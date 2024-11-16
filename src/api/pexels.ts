dd110091918f9b540dd8a19c3b40e6ad4f3d573e
beugene <eugene.bugor@gmail.com>
2024-11-16 22:02:11 +0100

    feat: add Pexels API integration (#6)
    - added axios instance for Pexels API
    - added base Pexels API requests: /curated, /search, /photo
    - added type definitions for Pexels API

---
+++ src/api/pexels.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import type {
+  GetCuratedPhotosParams,
+  GetPhotoParams,
+  GetPhotoResponse,
+  PhotosResponse,
+  SearchPhotosParams,
+} from '../types/pexels.ts';
+
+const pexelsApi = axios.create({
+  baseURL: 'https://api.pexels.com/v1',
+  headers: {
+    Authorization: import.meta.env.VITE_PEXELS_API_KEY,
+  },
+});
+
+export const searchPhotos = async (params: SearchPhotosParams): Promise<PhotosResponse> => {
+  const response = await pexelsApi.get<PhotosResponse>('/search', {
+    params: {
+      page: params.page || 1,
+      per_page: params.per_page || 20,
+      ...params,
+    },
+  });
+
+  return response.data;
+};
+
+export const getCuratedPhotos = async (params: GetCuratedPhotosParams): Promise<PhotosResponse> => {
+  const response = await pexelsApi.get<PhotosResponse>('/curated', {
+    params: {
+      page: params.page || 1,
+      per_page: params.per_page || 20,
+    },
+  });
+
+  return response.data;
+};
+
+export const getPhoto = async (params: GetPhotoParams): Promise<GetPhotoResponse> => {
+  const response = await pexelsApi.get(`/photos/${params.id}`);
+  return response.data;
+};
---
+++ src/types/pexels.ts
@@ -0,0 +1,50 @@
+export enum PhotoSize {
+  Original = 'original',
+  Large2x = 'large2x',
+  Large = 'large',
+  Medium = 'medium',
+  Small = 'small',
+  Portrait = 'portrait',
+  Landscape = 'landscape',
+  Tiny = 'tiny',
+}
+
+export type PhotoSizeType = `${PhotoSize}`;
+
+export interface Photo {
+  id: number;
+  width: number;
+  height: number;
+  url: string;
+  photographer: string;
+  src: { [key in PhotoSizeType]: string };
+  alt: string;
+}
+
+/* API */
+
+export interface SearchPhotosParams {
+  query: string;
+  page?: number;
+  per_page?: number;
+}
+
+export interface GetCuratedPhotosParams {
+  page?: number;
+  per_page?: number;
+}
+
+export interface GetPhotoParams {
+  id: number;
+}
+
+export interface PhotosResponse {
+  photos: Photo[];
+  page: number;
+  per_page: number;
+  next_page?: string;
+  prev_page?: string;
+  total_results: number;
+}
+
+export type GetPhotoResponse = Photo;
