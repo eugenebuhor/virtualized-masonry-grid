56dc2506875db6d24c1cf8eca1a89a76c8f76887
beugene <eugene.bugor@gmail.com>
2024-11-17 18:24:17 +0100

    feat: add Gallery page
    - added GalleryPage component with grid of photos
    - added page as an index route component

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,22 @@
+import { lazy, Suspense } from 'react';
 import { type RouteObject, Navigate } from 'react-router-dom';
 import MainLayout from './MainLayout';
+
+const GalleryPage = lazy(() => import('../pages/GalleryPage.tsx'));
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <MainLayout />,
     children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <GalleryPage />
+          </Suspense>
+        ),
+      },
       {
         path: '*',
         element: <Navigate to="/" replace />,
