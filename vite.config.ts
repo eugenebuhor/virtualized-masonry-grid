7dbaadca8d9de5e5e18eccff94b76c5e22177af4
beugene <eugene.bugor@gmail.com>
2024-11-18 04:34:42 +0100

    feat: vite build config setup
    - put potentially heavy dependencies into chunks
    - chose target 'modules' in favor of broader support yet optimized bundles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    target: 'modules',
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react';
+            }
+            if (id.includes('react-dom')) {
+              return 'react-dom';
+            }
+            if (id.includes('react-router-dom')) {
+              return 'react-router-dom';
+            }
+            if (id.includes('styled-components')) {
+              return 'styled-components';
+            }
+            if (id.includes('axios')) {
+              return 'axios';
+            }
+            if (id.includes('@tanstack/react-query')) {
+              return 'react-query';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'styled-components',
+      'axios',
+      '@tanstack/react-query',
+    ],
+  },
 });
